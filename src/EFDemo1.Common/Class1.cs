b37507e52917b6960231a9c375ecd9478ec1e9a4
Elijah Bate <elijahbate@gmail.com>
2019-04-03 23:19:59 +1100

    Updates

--- src/EFDemo1.Common/Class1.cs
+++ src/EFDemo1.Common/Class1.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
+using LiteDB;
+using NEvilES;
+using NEvilES.Pipeline;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace EFDemo1.Common
 {
     public class LocalEventStore : IAsyncRepository
     {
+        private readonly LiteDatabase _db;
         private readonly IEventTypeLookupStrategy _eventTypeLookupStrategy;
         private readonly CommandContext _commandContext;
         public LocalEventStore(
+            LiteDatabase db,
             IEventTypeLookupStrategy eventTypeLookupStrategy,
             CommandContext commandContext
             )
         {
+            _db = db;
             _eventTypeLookupStrategy = eventTypeLookupStrategy;
             _commandContext = commandContext;
         }
@@ -32,7 +41,35 @@ namespace EFDemo1.Common
         {
 
         }
-        Task<IAggregate> GetStatelessAsync(Type type, Guid id);
-        Task<AggregateCommit> SaveAsync(IAggregate aggregate);
+        public async Task<IAggregate> GetStatelessAsync(Type type, Guid id)
+        {
+
+        }
+        public async Task<AggregateCommit> SaveAsync(IAggregate aggregate)
+        {
+            // _db.GetCollection<EventDb>("events");
+
+            // var customer = new Customer
+            // {
+            // Name = "John Doe",
+            // Phones = new string[] { "8000-0000", "9000-0000" },
+            // Age = 39,
+            // IsActive = true
+            // };
+
+            // // Create unique index in Name field
+            // col.EnsureIndex(x => x.Name, true);
+
+            // // Insert new customer document (Id will be auto-incremented)
+            // col.Insert(customer);
+
+            // // Update a document inside a collection
+            // customer.Name = "Joana Doe";
+
+            // col.Update(customer);
+
+            // // Use LINQ to query documents (with no index)
+            // var results = col.Find(x => x.Age > 20);
+        }
     }
 }
